7bd8b4e0a4df541f1d44edf082d6f5465e89d49f
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-20 21:29:00 +0100

    Remove template example leftovers

--- src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneedscoordinatorapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessrisksandneedscoordinatorapi/config/WebClientConfiguration.kt
@@ -11,22 +11,11 @@ import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
-  @Value("\${example-api.url}") val exampleApiBaseUri: String,
-  @Value("\${hmpps-auth.url}") val hmppsAuthBaseUri: String,
+  @Value("\${hmpps-auth.base-url}") val hmppsAuthBaseUri: String,
   @Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
   @Value("\${api.timeout:20s}") val timeout: Duration,
 ) {
   // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
-  // TODO: Remove the health ping if no call outs to other services are made
   @Bean
   fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
-
-  // TODO: This is an example health bean for checking other services and should be removed / replaced
-  @Bean
-  fun exampleApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(exampleApiBaseUri, healthTimeout)
-
-  // TODO: This is an example bean for calling other services and should be removed / replaced
-  @Bean
-  fun exampleApiWebClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
-    builder.authorisedWebClient(authorizedClientManager, registrationId = "example-api", url = exampleApiBaseUri, timeout)
 }
