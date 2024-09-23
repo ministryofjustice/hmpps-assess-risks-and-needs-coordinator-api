a4e92775a8c6ac7f4f89c3c175b43f248cb10284
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-23 10:20:04 +0100

    Add configuration file to read application.yaml entries for SAN API

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/assessment/api/StrengthsAndNeedsApiProperties.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.assessment.api
+
+import jakarta.annotation.PostConstruct
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "app.services.strengths-and-needs-api")
+data class StrengthsAndNeedsApiProperties(
+  var baseUrl: String = "",
+  var endpoints: Endpoints = Endpoints(),
+  var paths: Endpoints = Endpoints()
+) {
+
+  data class Endpoints(
+    var fetch: String = "",
+    var create: String = ""
+  )
+
+  @PostConstruct
+  fun init() {
+    paths = Endpoints(
+      fetch = baseUrl + endpoints.fetch,
+      create = baseUrl + endpoints.create
+    )
+  }
+}
