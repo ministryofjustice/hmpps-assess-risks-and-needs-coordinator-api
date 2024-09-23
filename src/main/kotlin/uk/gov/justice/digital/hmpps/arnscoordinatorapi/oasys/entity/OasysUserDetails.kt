cea49eb646007cdce108a3d19d96c9169c6ba2a0
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-23 20:10:10 +0100

    Add OasysUserDetails entity

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/oasys/entity/OasysUserDetails.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.entity
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Size
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.config.Constraints
+
+data class OasysUserDetails(
+  @Schema(description = "User ID", example = "RB123XYZ")
+  @Size(max = Constraints.OASYS_USER_ID_MAX_LENGTH)
+  val id: String = "",
+
+  @Schema(description = "User name", example = "John Doe")
+  @Size(max = Constraints.OASYS_USER_NAME_MAX_LENGTH)
+  val name: String = "",
+)
