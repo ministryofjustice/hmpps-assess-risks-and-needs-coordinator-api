534303e1f160164f09e514d55ec2105fcd560eda
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-23 22:19:31 +0100

    ktlint fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/config/WebClientConfiguration.kt
@@ -3,9 +3,7 @@ package uk.gov.justice.digital.hmpps.arnscoordinatorapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.web.reactive.function.client.WebClient
-import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
 import java.time.Duration
 
