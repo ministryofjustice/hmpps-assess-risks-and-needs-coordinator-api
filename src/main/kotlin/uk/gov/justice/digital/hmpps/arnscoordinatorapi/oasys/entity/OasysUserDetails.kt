982c1e803541ddcedfc01682d4deb83dc881f194
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-23 10:20:23 +0100

    Add some generic types

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/oasys/entity/OasysUserDetails.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.entity
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Size
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.config.Constraints
+
+data class OasysUserDetails(
+  @Schema(description = "OASys User ID", example = "RB12XYZ")
+  @Size(max = Constraints.OASYS_USER_ID_MAX_LENGTH)
+  val id: String = "",
+
+  @Schema(description = "User's full name", example = "John Doe")
+  @Size(max = Constraints.OASYS_USER_NAME_MAX_LENGTH)
+  val name: String = "",
+)
