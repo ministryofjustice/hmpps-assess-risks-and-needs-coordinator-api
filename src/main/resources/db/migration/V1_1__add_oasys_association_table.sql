032b8bd8fe00fbcc9e8caa47df63f05bf53a4164
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-26 18:01:21 +0100

    Add OASys associations table

---
+++ src/main/resources/db/migration/V1_1__add_oasys_association_table.sql
@@ -0,0 +1,12 @@
+CREATE TYPE coordinator.entity_type AS ENUM ('ASSESSMENT', 'PLAN');
+
+CREATE TABLE coordinator.oasys_associations (
+    id SERIAL PRIMARY KEY,
+    uuid uuid NOT NULL,
+    created_at timestamp NOT NULL,
+    entity_uuid uuid NOT NULL,
+    entity_type coordinator.entity_type NOT NULL,
+    oasys_assessment_pk varchar(64) NOT NULL,
+    region_prison_code varchar(64),
+    deleted boolean DEFAULT false
+);
