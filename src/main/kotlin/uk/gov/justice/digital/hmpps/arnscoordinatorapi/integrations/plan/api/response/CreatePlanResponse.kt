327fdbb836fa7b196b68ea638d7cc79515d5be32
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-27 10:49:29 +0100

    Refactor CreatePlanResponse data type

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/plan/api/response/CreatePlanResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/plan/api/response/CreatePlanResponse.kt
@@ -1,9 +1,8 @@
 package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.plan.api.response
 
-import uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.common.entity.CreateResponse
 import java.util.*
 
 data class CreatePlanResponse(
   val sentencePlanId: UUID,
   val sentencePlanVersion: Long,
-) : CreateResponse
+)
