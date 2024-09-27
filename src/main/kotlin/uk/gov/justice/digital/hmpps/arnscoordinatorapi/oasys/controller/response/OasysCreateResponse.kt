f582be9464ee5b6099e54d7faba4a28b991b9a1f
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-27 11:44:23 +0100

    Added `addVersionedEntity` function to OasysCreateResponse to easily build response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/oasys/controller/response/OasysCreateResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/oasys/controller/response/OasysCreateResponse.kt
@@ -1,10 +1,27 @@
 package uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.response
 
-import java.util.*
+import com.fasterxml.jackson.annotation.JsonInclude
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.common.entity.VersionedEntity
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.associations.repository.EntityType
+import java.util.UUID
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class OasysCreateResponse(
-  val sanAssessmentId: UUID,
-  val sanAssessmentVersion: Int,
-  val sentencePlanId: UUID? = null,
-  val sentencePlanVersion: Int? = null,
-)
+  var sanAssessmentId: UUID? = null,
+  var sanAssessmentVersion: Long? = null,
+  var sentencePlanId: UUID? = null,
+  var sentencePlanVersion: Long? = null,
+) {
+  fun addVersionedEntity(versionedEntity: VersionedEntity) {
+    when (versionedEntity.entityType) {
+      EntityType.PLAN -> {
+        this.sentencePlanId = versionedEntity.id
+        this.sentencePlanVersion = versionedEntity.version
+      }
+      EntityType.ASSESSMENT -> {
+        this.sanAssessmentId = versionedEntity.id
+        this.sanAssessmentVersion = versionedEntity.version
+      }
+    }
+  }
+}
