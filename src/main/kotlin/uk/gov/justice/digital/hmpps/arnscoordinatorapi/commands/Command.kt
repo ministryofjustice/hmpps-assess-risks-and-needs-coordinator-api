5d4bc22182e0e22dd27857c300e139c4d81f2787
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-30 13:37:23 +0100

    Switch to using a Command pattern to make rollbacks easier

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/commands/Command.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.commands
+
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.common.entity.OperationResult
+
+interface Command {
+  fun execute(): OperationResult<*>
+
+  fun rollback(): OperationResult<Unit>
+}
