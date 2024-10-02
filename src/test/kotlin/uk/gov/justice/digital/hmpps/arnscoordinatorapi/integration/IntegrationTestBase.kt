905f0ce2cd34e6aa60f39232a00e7e6dc792b1cc
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-10-02 16:30:08 +0100

    SP2-815: Added standalone integration tests for create

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integration/IntegrationTestBase.kt
@@ -47,5 +47,6 @@ abstract class IntegrationTestBase {
   protected fun stubPingWithResponse(status: Int) {
     hmppsAuth.stubHealthPing(status)
     sanServer.stubHealthPing(status)
+    sentencePlanApiMock.stubHealthPing(status)
   }
 }
