1596593e5f20660aae549f5b16a6561fd5c681b9
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-08 15:03:41 +0100

    Remove unused file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/assessment/api/response/LockAssessmentResponse.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.assessment.api.response
-
-import java.util.UUID
-
-data class LockAssessmentResponse(
-  val sanAssessmentId: UUID,
-  val sanAssessmentVersion: Long,
-)
