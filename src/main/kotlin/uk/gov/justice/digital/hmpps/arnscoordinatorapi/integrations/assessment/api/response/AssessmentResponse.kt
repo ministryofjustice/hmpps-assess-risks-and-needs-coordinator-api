be6695bf71ce2174dd91d16592d783d6610a0af5
Dimitar Kokov <dm.kokov@gmail.com>
2024-10-08 13:14:08 +0100

    Update SAN endpoints and response objects (#45)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/assessment/api/response/AssessmentResponse.kt
@@ -0,0 +1,19 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.assessment.api.response
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class AssessmentResponse(
+  val metaData: AssessmentMetadata,
+  val assessment: Map<String, *>,
+)
+
+data class AssessmentMetadata(
+  val uuid: UUID,
+  val createdAt: LocalDateTime,
+  val versionUuid: UUID,
+  val versionNumber: Long,
+  val versionCreatedAt: LocalDateTime,
+  val versionUpdatedAt: LocalDateTime,
+  val formVersion: String?,
+)
