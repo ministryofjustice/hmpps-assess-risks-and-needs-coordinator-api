a76167f03771e6f7ca594e34970a0f5c8017dda0
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-09 14:11:37 +0100

    Add SignTest

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integration/SignTest.kt
@@ -0,0 +1,151 @@
+package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.common.entity.SignType
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.associations.repository.EntityType
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.associations.repository.OasysAssociation
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.associations.repository.OasysAssociationRepository
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.request.OasysGenericRequest
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.request.OasysSignRequest
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.response.OasysVersionedEntityResponse
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.entity.OasysUserDetails
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+import java.util.*
+
+class SignTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var oasysAssociationRepository: OasysAssociationRepository
+
+  @BeforeEach
+  fun setUp() {
+    stubGrantToken()
+    stubAssessmentsSign()
+    stubSentencePlanSign()
+  }
+
+  @Test
+  fun `it successfully signs an existing SP and SAN for an oasys PK`() {
+    val oasysAssessmentPk = "300"
+    oasysAssociationRepository.saveAll(
+      listOf(
+        OasysAssociation(
+          id = 1L,
+          oasysAssessmentPk = oasysAssessmentPk,
+          entityType = EntityType.PLAN,
+          entityUuid = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ),
+        OasysAssociation(
+          id = 2L,
+          oasysAssessmentPk = oasysAssessmentPk,
+          entityType = EntityType.ASSESSMENT,
+          entityUuid = UUID.fromString("4fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ),
+      ),
+    )
+
+    val response = webTestClient.post().uri("/oasys/$oasysAssessmentPk/sign")
+      .header(HttpHeaders.CONTENT_TYPE, "application/json")
+      .headers(setAuthorisation(roles = listOf("ROLE_STRENGTHS_AND_NEEDS_OASYS")))
+      .bodyValue(
+        OasysSignRequest(
+          signType = SignType.COUNTERSIGN,
+          userDetails = OasysUserDetails(id = "1", name = "Test Name"),
+        ),
+      )
+      .exchange()
+      .expectStatus().isOk
+      .expectBody(OasysVersionedEntityResponse::class.java)
+      .returnResult()
+      .responseBody
+
+    assertThat(response?.sanAssessmentId).isEqualTo(UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6"))
+    assertThat(response?.sanAssessmentVersion).isEqualTo(0)
+  }
+
+  @Test
+  fun `it returns a 409 when the SAN assessment is already signed`() {
+    stubAssessmentsSign(409)
+    val oasysAssessmentPk = "301"
+    oasysAssociationRepository.saveAll(
+      listOf(
+        OasysAssociation(
+          id = 1L,
+          oasysAssessmentPk = oasysAssessmentPk,
+          entityType = EntityType.ASSESSMENT,
+          entityUuid = UUID.fromString("5fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ),
+      ),
+    )
+
+    val response = webTestClient.post().uri("/oasys/$oasysAssessmentPk/sign")
+      .header(HttpHeaders.CONTENT_TYPE, "application/json")
+      .headers(setAuthorisation(roles = listOf("ROLE_STRENGTHS_AND_NEEDS_OASYS")))
+      .bodyValue(
+        OasysSignRequest(
+          signType = SignType.COUNTERSIGN,
+          userDetails = OasysUserDetails(id = "1", name = "Test Name"),
+        ),
+      )
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isEqualTo(409)
+      .expectBody(ErrorResponse::class.java)
+      .returnResult().responseBody
+
+    assertThat(response?.userMessage).isEqualTo("Failed to sign ASSESSMENT entity due to a conflict, Assessment already signed")
+  }
+
+  @Test
+  fun `it returns a 409 when the Sentence Plan is already locked`() {
+    stubSentencePlanSign(409)
+    val oasysAssessmentPk = "302"
+    oasysAssociationRepository.saveAll(
+      listOf(
+        OasysAssociation(
+          id = 1L,
+          oasysAssessmentPk = oasysAssessmentPk,
+          entityType = EntityType.PLAN,
+          entityUuid = UUID.fromString("5fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ),
+      ),
+    )
+
+    val response = webTestClient.post().uri("/oasys/$oasysAssessmentPk/sign")
+      .header(HttpHeaders.CONTENT_TYPE, "application/json")
+      .headers(setAuthorisation(roles = listOf("ROLE_STRENGTHS_AND_NEEDS_OASYS")))
+      .bodyValue(
+        OasysSignRequest(
+          signType = SignType.COUNTERSIGN,
+          userDetails = OasysUserDetails(id = "1", name = "Test Name"),
+        ),
+      )
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isEqualTo(409)
+      .expectBody(ErrorResponse::class.java)
+      .returnResult().responseBody
+
+    assertThat(response?.userMessage).isEqualTo("Failed to sign PLAN entity due to a conflict, Sentence Plan is already signed")
+  }
+
+  @Test
+  fun `it returns a 404 when no associations found`() {
+    webTestClient.post().uri("/oasys/999/lock")
+      .header(HttpHeaders.CONTENT_TYPE, "application/json")
+      .headers(setAuthorisation(roles = listOf("ROLE_STRENGTHS_AND_NEEDS_OASYS")))
+      .bodyValue(
+        OasysGenericRequest(
+          userDetails = OasysUserDetails(id = "1", name = "Test Name"),
+        ),
+      )
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isNotFound
+  }
+}
