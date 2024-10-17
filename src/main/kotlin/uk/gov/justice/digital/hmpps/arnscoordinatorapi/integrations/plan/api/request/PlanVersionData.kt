522d54e6829d9a194f2c60c9e3b8439cf53eba5b
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-17 11:23:08 +0100

    Fix issue with rollback on SP

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/plan/api/request/PlanVersionData.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/plan/api/request/PlanVersionData.kt
@@ -1,14 +1,16 @@
 package uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.plan.api.request
 
+import uk.gov.justice.digital.hmpps.arnscoordinatorapi.integrations.common.entity.UserDetails
 import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.request.OasysRollbackRequest
 
 data class PlanVersionData(
   val sentencePlanVersion: Long,
+  val userDetails: UserDetails
 ) {
   companion object {
     fun from(request: OasysRollbackRequest) =
       request.sentencePlanVersionNumber
-        ?.let { PlanVersionData(it) }
+        ?.let { PlanVersionData(it, request.userDetails.intoUserDetails()) }
         ?: throw Exception("Unable to construct rollback request data. Missing sentencePlanVersionNumber.")
   }
 }
