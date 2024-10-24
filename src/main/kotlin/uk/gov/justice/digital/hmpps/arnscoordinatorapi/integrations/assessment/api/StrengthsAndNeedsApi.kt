ae14de9b2ec3f5020b0a534f2710913013261069
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-24 15:56:07 +0100

    Fix error messages

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/assessment/api/StrengthsAndNeedsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/integrations/assessment/api/StrengthsAndNeedsApi.kt
@@ -230,14 +230,14 @@ class StrengthsAndNeedsApi(
 
       result?.let {
         ApiOperationResultExtended.Success(it)
-      } ?: throw IllegalStateException("Unexpected error during soft-delete")
+      } ?: throw IllegalStateException("Unexpected error during soft-delete assessment versions")
     } catch (ex: WebClientResponseException) {
       if (ex.statusCode.value() == HttpStatus.CONFLICT.value()) {
         return ApiOperationResultExtended.Conflict("Unable to soft-delete the requested assessment versions: ${ex.responseBodyAsString}")
       }
-      ApiOperationResultExtended.Failure("HTTP error during soft-delete: Status code ${ex.statusCode}, Response body: ${ex.responseBodyAsString}")
+      ApiOperationResultExtended.Failure("HTTP error during soft-delete assessment versions: Status code ${ex.statusCode}, Response body: ${ex.responseBodyAsString}")
     } catch (ex: Exception) {
-      ApiOperationResultExtended.Failure("Unexpected error during soft-delete: ${ex.message}", ex)
+      ApiOperationResultExtended.Failure("Unexpected error during softDeleteAssessment: ${ex.message}", ex)
     }
   }
 
