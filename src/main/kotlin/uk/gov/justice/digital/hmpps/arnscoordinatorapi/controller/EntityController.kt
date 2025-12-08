9ffba30b01d5749fc133f628391071cfd4a0ee78
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-12-08 11:32:27 +0000

    Revert "Fix versions endpoint response for swagger doc (#137)"
    This reverts commit 54e2c32fd2661b22ee7661e01ec8f16edf6be49d.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/controller/EntityController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnscoordinatorapi/controller/EntityController.kt
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.arnscoordinatorapi.controller.response.VersionsResponse
 import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.OasysCoordinatorService
 import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.associations.repository.EntityType
 import uk.gov.justice.digital.hmpps.arnscoordinatorapi.oasys.controller.response.OasysGetResponse
@@ -39,7 +38,7 @@ class EntityController(
       ApiResponse(
         responseCode = "200",
         description = "Entities found",
-        content = arrayOf(Content(schema = Schema(implementation = VersionsResponse::class))),
+        content = arrayOf(Content(schema = Schema(implementation = OasysGetResponse::class))),
       ),
       ApiResponse(
         responseCode = "404",
